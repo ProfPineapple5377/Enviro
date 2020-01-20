41f6ed13470b317933e88485979d4bc72bf7c836
Jess Mayers <jbmayers22@gmail.com>
2020-01-19 22:10:37 -0500

    important links => sources

--- src/app/enviro.component.ts
+++ src/app/enviro.component.ts
@@ -36,8 +36,15 @@ export class EnviroApp {
 }
 /*
   Data sets backup:
-    https://data.cdp.net/resource/marp-zazk.json
-    https://data.cdp.net/resource/4hek-p74b.json
-    https://data.cdp.net/resource/5fxz-29mk.json
-    https://data.cdp.net/resource/tsqv-bik6.json
+    link: https://data.cdp.net/Companies/2013-Global-500-Emissions-and-Response-Status/marp-zazk
+    api: https://data.cdp.net/resource/marp-zazk.json
+
+    link: https://data.cdp.net/Companies/2012-Global-500-Emissions-and-Response-Status/4hek-p74b
+        api: https://data.cdp.net/resource/4hek-p74b.json
+
+    link: https://data.cdp.net/Companies/2014-Forests-Risk-Assessment/5fxz-29mk
+        api: https://data.cdp.net/resource/5fxz-29mk.json
+
+    link: 
+        api: https://data.cdp.net/resource/tsqv-bik6.json
 */
